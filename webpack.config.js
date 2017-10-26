7b5943ea67e87f711837de57aadeae3c9f2bf476
MrDev023 <trexricher1997@gmail.com>
2017-10-26 16:01:08 +0200

    Add mocha test,css-loader,autoprefixer,js

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,45 @@ const path = require("path")
 const CleanWebpackPlugin = require('clean-webpack-plugin') // Supprime les dossier voulus entre chaque compilation
 const HtmlWebpackPlugin = require('html-webpack-plugin') // Genere les fichiers HTML
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin') // Minimize le js
-const ManifestWebpackPlugin = require('manifest-webpack-plugin') // Genere le fichier Manifest avec tout les fichiers source à implementer avec HtmlWebpackPlugin
+const ManifestWebpackPlugin = require('webpack-manifest-plugin') // Genere le fichier Manifest avec tout les fichiers source à implementer avec HtmlWebpackPlugin
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const dev = process.env.NODE_ENV === "dev"
 
+let cssLoaders = [
+    {loader:'css-loader',options:{importLoaders:1,minimize: !dev}},
+    {
+        loader:'postcss-loader',
+        options: {
+          plugins: (loader) => [
+            require('autoprefixer')({
+                browsers: ['last 2 versions', 'ie > 8']
+            }),
+          ]
+        }
+    }
+] 
+
 module.exports = {
-    entry: './src/js/app.js',
+    entry: {
+        app : ['./src/css/app.scss','./src/js/app.js']
+    },
     watch: dev,
+    devtool: dev ? "cheap-module-eval-source-map" : false,
     output: {
         path: path.resolve(__dirname, 'public'),
         publicPath: '/',
         filename: '[name].[chunkhash:16].js'
+    },
+    resolve: {
+        alias: {
+            '@css': path.resolve('./src/css/'),
+            '@': path.resolve('./src/js/')
+        }
+    },
+    devServer: {
+        contentBase: path.resolve('./public'),
+        overlay: true
     },
     module: {
         rules: [
@@ -40,18 +68,56 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: cssLoaders
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use:[...cssLoaders,"sass-loader"]
+                })
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name:'[name].[hash:16].[ext]'
+                        }
+                    },
+                    {
+                        loader: 'img-loader',
+                        options: {
+                            enabled: !dev
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
-        new ManifestWebpackPlugin(path.join('public', 'manifest.json')),
+        new ExtractTextPlugin({
+            filename: '[name].[contenthash:16].css'
+        }),
+        new ManifestWebpackPlugin(),
         new CleanWebpackPlugin(['public'], {
             dry: false,
             verbose: true,
             root: path.resolve('./')
         }),
         new HtmlWebpackPlugin({
-            title: "Test Webpack :P",
             template: 'template/index.hbs',
             //hash: false, // Inutile dans notre cas
             minify:{
